f0bceb7a033c5ed47304db0c994d8112c98fecef
Leviatas <leviatas@gmail.com>
2025-10-16 14:14:03 -0300

    Add Inversion Of Control Proeyct

---
+++ Leviatas.MicroRabbit.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,15 @@
+﻿using Leviatas.MicroRabbit.Domain.Core.Bus;
+using Leviatas.MicroRabbit.Infra.Bus;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Leviatas.MicroRabbit.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            // Here we will register all our dependencies
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
