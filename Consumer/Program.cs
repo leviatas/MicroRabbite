d17cba6d6b8043980848504314d2b639736ddfb4
Leviatas <leviatas@gmail.com>
2025-10-16 13:29:11 -0300

    Add project files.

---
+++ Consumer/Program.cs
@@ -0,0 +1,38 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Consumer
+{
+    internal class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using var connection = await factory.CreateConnectionAsync();
+            using var channel = await connection.CreateChannelAsync();
+
+            const string queueName = "BasicTest";
+            await channel.QueueDeclareAsync(queue: queueName,
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+            var consumer = new AsyncEventingBasicConsumer(channel);
+
+            consumer.ReceivedAsync += async (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = System.Text.Encoding.UTF8.GetString(body);
+                Console.WriteLine("Received message: {0}", message);
+                await Task.Yield();
+            };
+
+
+            await channel.BasicConsumeAsync(queueName, true, consumer);
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Practice2/Program.cs
@@ -0,0 +1,35 @@
+﻿using RabbitMQ.Client;
+
+namespace Practice2
+{
+    internal class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using var connection = await factory.CreateConnectionAsync();
+            using var channel = await connection.CreateChannelAsync();
+
+            const string queueName = "BasicTest";
+            await channel.QueueDeclareAsync(queue: queueName,
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+            string message = "Getting Started with .NET Core RabbitMQ";
+            var body = System.Text.Encoding.UTF8.GetBytes(message);
+
+            await channel.BasicPublishAsync(
+                     exchange: "",
+                     routingKey: queueName,
+                     mandatory: false,
+                     body: body);
+
+            Console.WriteLine(" [x] Sent {0}", message);
+            Console.WriteLine("Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
